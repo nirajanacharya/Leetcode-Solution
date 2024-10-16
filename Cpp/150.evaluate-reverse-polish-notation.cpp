7bbcdde6f42f11dc49640f9d1d8b6e1e8e392aa7
Xtha-Sunil <kodersunil777@gmail.com>
2024-10-16 18:03:27 +0545

    Added 150.evaluate reverse polish notation via upload.

---
+++ Cpp/150.evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        int ans = 0;
+        stack<int> s;
+        for(int i=0;i<tokens.size();i++)
+        {
+           if(tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/")
+           {
+               int x = s.top();
+               s.pop();
+               int y = s.top();
+               s.pop();
+               if(tokens[i] == "+") s.push(x + y);
+               if(tokens[i] == "-") s.push(y - x);
+               if(tokens[i] == "*") s.push(x*y);
+               if(tokens[i] == "/") s.push(y/x);
+           }
+            else
+                s.push(stoi(tokens[i]));
+        }
+        
+        return s.top();
+    }
+};
