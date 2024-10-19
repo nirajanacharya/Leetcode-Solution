d9549808f68e6a56ab2b2a3ae87e48f53a6a6097
Nirajan Acharya <87110903+nirajanacharya@users.noreply.github.com>
2024-10-19 12:47:59 +0545

    Create 275.h_index_ii.cpp

---
+++ Cpp/275.h_index_ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        int low = 0, high = n - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (citations[mid] == n - mid) {
+                return n - mid;
+            } else if (citations[mid] < n - mid) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return n - low;
+    }
+};
